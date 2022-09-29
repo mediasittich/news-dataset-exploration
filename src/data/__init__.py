f0a3c3b1cc1ce2e4597d9a0d7d873fab270896dc
Regina Galambos <gina.galambos@gmail.com>
2022-09-29 21:15:06 +0200

    write cleaning functions

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,1 +1,4 @@
 from .make_dataset import make_csv_from_csvs
+from .clean_text import remove_numbers, remove_hash, \
+    remove_urls_email, remove_emojis, remove_punctuation, remove_whitespace, \
+    clean_txt, calculate_word_count
---
+++ src/data/clean_text.py
@@ -0,0 +1,36 @@
+import re
+import string
+import emoji
+
+def remove_numbers(txt):
+    return re.sub(r"\d+", '', txt)
+
+def remove_hash(txt):
+    return re.sub(r"#", '', txt)
+
+def remove_urls_email(txt):
+    clean_str = re.sub(r"\S*@\S*\s?", '', txt)
+    clean_str = re.sub(r"https?://[^\s\n\r]+", '', clean_str)
+    return clean_str
+
+def remove_emojis(txt):
+    return emoji.replace_emoji(txt, replace='')
+
+def remove_punctuation(txt):
+    return txt.translate(str.maketrans('', '', string.punctuation))
+
+def remove_whitespace(txt):
+    return ' '.join(re.sub('[\s+]', ' ', txt).split())
+
+def clean_txt(txt):
+    clean_txt = remove_numbers(txt)
+    clean_txt = remove_hash(clean_txt)
+    clean_txt = remove_urls_email(clean_txt)
+    clean_txt = remove_emojis(clean_txt)
+    clean_txt = remove_punctuation(clean_txt)
+    clean_txt = remove_whitespace(clean_txt)
+    return clean_txt
+
+def calculate_word_count(txt):
+    return len(txt.split(' '))
+
