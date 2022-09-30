e50c25246cee38db829e6466182c8a27743e512a
Regina Galambos <gina.galambos@gmail.com>
2022-10-01 00:01:42 +0200

    finish exploring NYT data, write cleaning script for NYT

---
+++ src/main.py
@@ -0,0 +1,60 @@
+import os
+import glob
+import pandas as pd
+
+#from src.data.make_dataset import make_csv_from_csvs, make_csv_from_jsons
+from src.data.clean_text import clean_txt, calculate_word_count
+
+DATA_DIR = os.path.join(os.getcwd(), 'data')
+RAW_DATA_DIR = os.path.join(os.getcwd(), 'data/raw')
+
+NYT_DATA_DIR = os.path.join(RAW_DATA_DIR, 'nyt_headlines')
+GUARDIAN_DATA_DIR = os.path.join(RAW_DATA_DIR, 'guardian')
+TWITTER_GU_DATA_DIR = os.path.join(RAW_DATA_DIR, 'gu_twitter')
+
+if not os.path.exists(os.path.join(DATA_DIR, 'interim')):
+    os.mkdir(os.path.join(DATA_DIR, 'interim'))
+INTERIM_DIR = os.path.join(DATA_DIR, 'interim')
+
+if not os.path.exists(os.path.join(DATA_DIR, 'processed')):
+    os.mkdir(os.path.join(DATA_DIR, 'processed'))
+PROCESSED_DIR = os.path.join(DATA_DIR, 'processed')
+
+#############################################################
+#                   NYT Dataset
+#############################################################
+# Load NYT data
+nyt_files = glob.glob(os.path.join(NYT_DATA_DIR, '*.csv'))
+
+for file in nyt_files:
+    print(f"Processing file {file} ...")
+    nyt_df = pd.read_csv(file)
+
+    # select only doc_type == article & material_type == News
+    nyt_df = nyt_df[(nyt_df['doc_type'] == 'article') & (nyt_df['material_type'] == 'News')]
+    # reset index on dataframe
+    nyt_df.reset_index(drop=True, inplace=True)
+    # drop columns: 'doc_type', 'material_type', 'snippet', 'lead_paragraph'
+    nyt_df.drop(['doc_type', 'material_type', 'snippet', 'lead_paragraph'], axis=1, inplace=True)
+
+    # clean up headline and abstract
+    nyt_df['cl_headline'] = nyt_df.apply(lambda x: clean_txt(x['headline']), axis=1)
+    nyt_df['cl_abstract'] = nyt_df.apply(lambda x: clean_txt(x['abstract']), axis=1)
+    nyt_df.drop(['headline', 'abstract'], axis=1, inplace=True)
+
+    # calculate word counts for both columns
+    nyt_df['word_count_headline'] = nyt_df.apply(lambda x: calculate_word_count(x['cl_headline']), axis=1)
+    nyt_df['word_count_abstract'] = nyt_df.apply(lambda x: calculate_word_count(x['cl_abstract']), axis=1)
+
+    # export to interim folder
+    if not os.path.exists(os.path.join(INTERIM_DIR, 'nyt_data')):
+        os.mkdir(os.path.join(INTERIM_DIR, 'nyt_data'))
+    # create filename from input file name
+    file_name = os.path.basename(file)
+    # save to subdir in interim data folder 
+    nyt_df.to_csv(os.path.join(INTERIM_DIR, f"nyt_data/{file_name}"), index=False)
+
+
+#############################################################
+#                   GUARDIAN Dataset
+#############################################################
